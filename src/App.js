bf216742dcd11eb06658b8871f390585c434947e
Raiza Leal Franca <raizale@hotmail.com>
2021-01-08 11:12:44 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import React from 'react';
 import Home from './components/Home.js';
 import './App.css';
+import Filtro from './components/Filtro.js';
 
-function App() {
+export default class App extends React.Component {
+
+  state = {
+    produtos: [
+        {
+            id: '1',
+            name: "Nome do Produto",
+            value: 3333,
+            imageUrl: "https://picsum.photos/200/300?random=1"
+        },
+        {
+            id: '2',
+            name: "Nome do Produto",
+            value: 2222,
+            imageUrl: "https://picsum.photos/200/300?random=2"
+        },
+        {
+            id: '3',
+            name: "Nome do Produto",
+            value: 1111,
+            imageUrl:  "https://picsum.photos/200/300?random=3"
+        },
+        {
+            id: '4',
+            name: "Nome do Produto",
+            value: 4444,
+            imageUrl:  "https://picsum.photos/200/300?random=4"
+        },
+        {
+            id: '5',
+            name: "Nome do Produto",
+            value: 8888,
+            imageUrl:  "https://picsum.photos/200/300?random=5"
+        },
+        {
+            id: '6',
+            name: "Nome do Produto",
+            value: 6666,
+            imageUrl:  "https://picsum.photos/200/300?random=6"
+        },
+        {
+            id: '7',
+            name: "Nome do Produto",
+            value: 5555,
+            imageUrl:  "https://picsum.photos/200/300?random=7"
+        },
+        {
+            id: '8',
+            name: "Nome do Produto",
+            value: 7777,
+            imageUrl:  "https://picsum.photos/200/300?random=8"
+        },
+    ],
+    filtro: "crescente",
+    data: "",
+    inputValueMin:0,
+    inputValueMin:1000,
+    inputValueText:'',
+    filtroInput:false
+  }
+  render(){
+  
   return (
     <div>
           <Home />
+          <Filtro produtos={this.state.produtos} min={this.state.produtos.inputValueMin} max={this.state.produtos.inputValueMax} />
     </div>
   );
 }
+}
 
-export default App;
---
+++ src/components/Filtro.js
@@ -0,0 +1,83 @@
+import React from "react";
+import './Filtro.css';
+
+
+export default class Filtro extends React.Component {
+
+  onChangeInputMin = (event) => {
+    this.setState({
+      inputValueMin: event.target.value
+    });
+  };
+  onChangeInputMax = (event) => {
+    this.setState({
+      InputValueMax: event.target.value
+    });
+  };
+  onChangeInputText = (event) => {
+    this.setState({
+      InputValueText: event.target.value
+    });
+  };
+
+
+
+
+
+  render() {
+
+    const listaFiltrada = this.props.produtos.filter((produto) => {
+
+      if (produto.value > this.valueMin) {
+        return true
+      } else {
+        return false
+      }
+    }).filter((produto) => {
+
+      if (produto.Text < this.valueMax) {
+        return true
+      } else {
+        return false
+      }
+    });;
+
+    console.log(listaFiltrada)
+
+    return (
+      <div>
+        <div class='containerFiltro'>
+          <div class='inputFiltro'>
+
+
+            <h3>Filtros:</h3>
+
+            <ul>{listaFiltrada.map(produto => {
+              return <li>{produto}</li>
+            })}</ul>
+
+            <label for="valorMin">Valor Mínimo:</label>
+            <input valueMin={props.state.inputValueMin}
+              onChange={this.onChangeInputMin}
+              type='number' />
+          </div>
+          <div class='inputFiltro'>
+            <label for="valorMax">Valor Máximo:</label>
+            <input valueMax={this.state.InputValueMax}
+              onChange={this.onChangeInputMax} type='number' />
+          </div>
+          <div class='inputFiltro'>
+            <label>Buscar Produtos</label>
+            <input valueText={this.state.InputValueText}
+              onChange={this.onChangeInputText}
+              type="text"
+              name="busca" />
+          </div>
+
+        </div>
+
+
+      </div >
+    );
+  }
+}
